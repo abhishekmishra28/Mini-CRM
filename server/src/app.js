72a4beefc99e807012c8a774ceea4b679f6424be
abhishekmishra28 <kumarabhishekmishra28@gmail.com>
2026-06-15 10:45:02 +0530

    Fixed cors error

--- server/src/app.js
+++ server/src/app.js
@@ -11,7 +11,10 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://mini-crm-iota-ten.vercel.app",
+    ],
     credentials: true,
   })
 );
